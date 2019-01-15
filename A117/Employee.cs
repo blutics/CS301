ec291105f1fa57654c50386e7d9685dcb18034a0
blutics <blutics@gmail.com>
2019-01-14 15:26:04 -1000

    'first'

---
+++ A117/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A117
+{
+    public class Employee
+    {
+        string name;
+        int id;
+        int age;
+        string job;
+        int year;
+        public Employee(string data)
+        {
+            string[] tmp = data.Split('|');
+            name = tmp[0];
+            id = int.Parse(tmp[1]);
+            age = int.Parse(tmp[2]);
+            job = tmp[3];
+            year = int.Parse(tmp[4]);
+        }
+        public void print()
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine(String.Format("|{0,-10:s} : {1,-20:s}|", "Name", name));
+            Console.WriteLine(String.Format("|{0,-10:s} : {1,-20:d}|", "ID", id));
+            Console.WriteLine(String.Format("|{0,-10:s} : {1,-20:d}|", "Age", age));
+            Console.WriteLine(String.Format("|{0,-10:s} : {1,-20:s}|", "Job", job));
+            Console.WriteLine(String.Format("|{0,-10:s} : {1,-20:d}|", "year", year));
+            Console.WriteLine("===================================");
+        }
+    }
+}
---
+++ A117/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace A117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("test.txt");
+            int n = int.Parse(file.ReadLine());
+            Employee[] employeelist = new Employee[n];
+            for(int i = 0; i < n; i++)
+            {
+                employeelist[i] = new Employee(file.ReadLine());
+                employeelist[i].print();
+            }
+        }
+    }
+}
