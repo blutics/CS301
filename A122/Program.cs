18e9b92f5ec2043d092ad407484797d48e92790a
blutics <blutics@gmail.com>
2019-01-18 13:19:11 -1000

    A122 v3 command version

--- A122/Program.cs
+++ A122/Program.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+//potential running command : A122.exe roster100.txt queries_linear.txt
+//running command with manual file : A122.exe input.txt query.txt
 namespace A122
 {
     class Program
@@ -15,8 +16,8 @@ namespace A122
             int current = 0;
             try
             {
-                //StreamReader file = new StreamReader(args[0]);//SteamReader will be used for reading text from file
-                StreamReader file = new StreamReader("roster100.txt");
+                StreamReader file = new StreamReader(args[0]);//SteamReader will be used for reading text from file
+                //StreamReader file = new StreamReader("roster100.txt");
                 current = 1;
                 int n = int.Parse(file.ReadLine());
                 Employee[] employeelist = new Employee[n];//making a array for employee objects
@@ -24,8 +25,8 @@ namespace A122
                 {
                     employeelist[i] = new Employee(file.ReadLine());//init!
                 }
-                //StreamReader query = new StreamReader(args[1]);
-                StreamReader query = new StreamReader("queries_linear.txt");
+                StreamReader query = new StreamReader(args[1]);
+                //StreamReader query = new StreamReader("queries_linear.txt");
                 int m = int.Parse(query.ReadLine());
                 for(int i = 0; i < m; i++)
                 {
